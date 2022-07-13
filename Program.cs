80e4e7dad52c9cc141773262c0d4f951a21f6af9
Kirill <krl.gushchinn@mail.ru>
2022-07-13 07:51:35 -0700

    Готовое задание №2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿//Программа определяющая какое число больше
+using System;
+class Sample
+{
+    public static void Main(){
+          int a, b, max;
+        Console.Write ("Введите число a ");
+        a=int.Parse(Console.ReadLine());
+        Console.Write ("Введите число b ");
+        b=int.Parse(Console.ReadLine());
+            max=0;
+
+ 
+            if (a>b)
+            {
+                max=a;
+
+            }
+            else
+            {
+                 max=b;
+
+            }
+                Console.WriteLine($"max= {max}");
+    }
+    }
